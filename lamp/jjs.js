d55108eacfab5f4513334433a18e75371fe05a64
vpatil1555 <73878432+vpatil1555@users.noreply.github.com>
2023-03-14 15:23:36 +0530

    Delete lamp directory

--- lamp/jjs.js
+++
@@ -1,7 +0,0 @@
-var btn = document.getElementById("btn");
-        var light = document.getElementById("light");
-
-        function togglebtn(){
-            btn.classList.toggle("active");
-            light.classList.toggle("on");
-        }
